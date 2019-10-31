362dcab359a77772994b9852ca579d5ce23cf30d
Thomas Trividic <thomas.trividic@prestashop.com>
2019-10-31 17:16:25 +0100

    more simple syntax

--- lib/index.ts
+++ lib/index.ts
@@ -4,10 +4,10 @@
  * @param  {} encoding a string representing the encoding see "Buffer.from" from Node.js documentation
  * @Return {object}
  */
-export function parseJsonFromBuffer(jsonBuffer: ArrayBuffer | SharedArrayBuffer, encoding:  "ascii" | "utf8" | "utf-8" | "utf16le" | "ucs2" | "ucs-2" | "base64" | "latin1" | "binary" | "hex") {
+export function parseJsonFromBuffer(jsonBuffer: string | ArrayBuffer | SharedArrayBuffer , encoding:  "ascii" | "utf8" | "utf-8" | "utf16le" | "ucs2" | "ucs-2" | "base64" | "latin1" | "binary" | "hex") {
     try {
-        const strBuffer = Buffer.from(jsonBuffer).toString()
-        const stringResult = Buffer.from(strBuffer,encoding).toString() // transform the buffer to string with specific encoding
+        const str = jsonBuffer.toString()
+        const stringResult = Buffer.from(str,encoding).toString() // transform the buffer to string with specific encoding
         const jsObject = JSON.parse(stringResult);
         return jsObject;
     } catch (error) {
