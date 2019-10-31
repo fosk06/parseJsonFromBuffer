5d1e074a8193a6581f58e14799f4ec8e634c62c8
Thomas Trividic <thomas.trividic@prestashop.com>
2019-10-31 17:23:56 +0100

    add error gestion

--- lib/index.ts
+++ lib/index.ts
@@ -6,7 +6,7 @@
  */
 export function parseJsonFromBuffer(jsonBuffer: string | ArrayBuffer | SharedArrayBuffer , encoding:  "ascii" | "utf8" | "utf-8" | "utf16le" | "ucs2" | "ucs-2" | "base64" | "latin1" | "binary" | "hex") {
     try {
-        if(!encoding) {
+        if(typeof encoding !== 'string') {
            throw new Error('encoding is mandatory');
         }
         const str = jsonBuffer.toString()
